a51618fc358f01cc7c02781f9174ddc02213a386
Zeyad Daowd <66565077+Zeyad-Daowd@users.noreply.github.com>
2024-04-30 16:08:43 +0300

    Phase 2

--- process.c
+++ process.c
@@ -1,20 +1,73 @@
 #include "headers.h"
 
+
 /* Modify this file as needed*/
 int remainingtime;
+int semId; // semaphore id
+int memId; // memory id
+int * remAddr;
+int startingIndex;
+int endingIndex;
+bool started;
 
-int main(int agrc, char * argv[])
+void handler(int signum);
+void getSem();
+void getMem();
+
+int main(int argc, char * argv[])
 {
     initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
+    signal(SIGUSR1, handler);
+    getMem();
+    getSem();
+    // TODO it needs to get the remaining time from somewhere
+    remainingtime = atoi(argv[1]); // initialized when initializing process
+    startingIndex = atoi(argv[2]); // initialized when initializing process
+    endingIndex = atoi(argv[3]); // initialized when initializing process
+
+    (*remAddr) = remainingtime;
+    printf("I am here process with remaining time: %d\n", remainingtime);
+    started = false;
+    up(semId);
+    // current thoughts -> wait for schedular to send a signal and decrease runtime
+    while (remainingtime > 0 || !started)
     {
-        // remainingtime = ??;
+        // just waits for signals to update the remaining time
     }
-    
+    // last thing
     destroyClk(false);
-    
+    // It should notify the schedular 
+    // current thoughts -> schedular binds SIGCHLD and if called it is terminated then
+    printf("Dying.\n");
     return 0;
+}
+void handler(int signum) {
+    started = true;
+    remainingtime--;
+    (*remAddr) = remainingtime;
+    printf("Remaining time decremented %d\n", *remAddr);
+    up(semId);
+    // re-bind
+    signal(SIGUSR1, handler);    
+}
+
+void getSem() {
+    semId = semget(SEMKEY, 1, 0666);
+
+}
+
+void getMem() {
+    memId = shmget(MEM_KEY,sizeof(int),0666);
+    if(memId == -1){
+        printf("Error in getting memory process.\n");
+    }
+
+    remAddr = shmat(memId, NULL, 0);
+    if (remAddr == (void *)-1) {
+        perror("shmat");
+    }
+}
+
+void destroyMem() {
+    shmdt(remAddr);
 }
